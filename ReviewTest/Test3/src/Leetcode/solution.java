563f924333371487e755277fac4c59dea35747f3
Nero951 <zhlwd103511@gmail.com>
2021-01-02 20:40:23 +0800

    ReviewTest

--- ReviewTest/Test3/src/Leetcode/solution.java
+++ ReviewTest/Test3/src/Leetcode/solution.java
@@ -1,10 +1,5 @@
 package Leetcode;
 
-import java.util.List;
-
 public class solution {
-    public static List<Integer> listReverse(List<Integer> list){
-        
-        return list;
-    }
+    
 }
