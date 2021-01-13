3355a12d2576fc7e3925f76d1eb69d9fc77d0103
Nero951 <zhlwd103511@gmail.com>
2021-01-13 19:32:26 +0800

    ReviewDemo

--- ReviewDemo/LeetCode/src/solution/Main.java
+++ ReviewDemo/LeetCode/src/solution/Main.java
@@ -1,8 +1,19 @@
 package solution;
 
+import java.util.Scanner;
+
 /**
  * @author: Wade
  * @create: 2021/1/11
  */
 public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNext()){
+            int num = scan.nextInt();
+            TreeNode root = new TreeNode(num);
+            BalanceTree b = new BalanceTree();
+            b.isBalance(root);
+        }
+    }
 }
