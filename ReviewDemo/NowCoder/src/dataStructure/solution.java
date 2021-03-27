6e76b968eb4082031931ccfc9ca9ea03c7948f35
Nero951 <zhlwd103511@gmail.com>
2021-03-27 15:57:59 +0800

    reviewdemo

--- ReviewDemo/NowCoder/src/dataStructure/solution.java
+++ ReviewDemo/NowCoder/src/dataStructure/solution.java
@@ -5,16 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class solution {
-    public static void maxWaterCap(){
+    public static void maxWaterCap() {
 
     }
+
     public String[] getFolderNames(String[] names) {
 
         return null;
     }
 
-    public List<Integer> findRepeatNumber(int[] arr){
-        if(arr.length==0) return null;
+    public List<Integer> findRepeatNumber(int[] arr) {
+        if (arr.length == 0) return null;
         List<Integer> list = new ArrayList<>();
         List<Integer> tmpList = new ArrayList<>();
         for (int j : arr) {
@@ -26,8 +27,23 @@ public class solution {
         return tmpList;
     }
 
-    public int cuttingRop(int n, int m){
+    public int cuttingRop(int n, int m) {
 
         return 0;
+    }
+
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length - 1;
+        int ans = 0;
+        while (l < r) {
+            int area = Math.min(height[l], height[r]) * (r - l);
+            ans = Math.max(ans, area);
+            if (height[l] <= height[r]) {
+                ++l;
+            } else {
+                --r;
+            }
+        }
+        return ans;
     }
 }
