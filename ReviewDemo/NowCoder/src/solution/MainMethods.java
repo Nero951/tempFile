4d927d1f6c1f5af29fb2bc2da85c47d297d9a1c2
Nero951 <zhlwd103511@gmail.com>
2021-04-19 09:40:41 +0800

    reviewdemo

--- ReviewDemo/NowCoder/src/solution/MainMethods.java
+++ ReviewDemo/NowCoder/src/solution/MainMethods.java
@@ -5,5 +5,18 @@ package solution;
  * @create: 2021/1/10
  */
 public class MainMethods {
+    public static void main(String[] args) {
+        try{
+            int n = 10/0;
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }finally {
+            Throwable t = new Throwable();
+            System.out.println(t.getMessage());
+            t.getCause();
+            System.out.println(t.getClass());
+            t.getStackTrace();
 
+        }
+    }
 }
