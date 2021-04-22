eee20fff25881f91fe908fe8834a4103490e7e78
Nero951 <zhlwd103511@gmail.com>
2021-04-22 22:05:38 +0800

    reviewdemo

--- ReviewDemo/NowCoder/src/solution/MainMethods.java
+++ ReviewDemo/NowCoder/src/solution/MainMethods.java
@@ -8,18 +8,14 @@ import java.util.*;
  */
 public class MainMethods {
     public static void main(String[] args) {
-        try{
-            int n = 10/0;
-        }catch (Exception e){
-            throw new RuntimeException(e);
-        }finally {
-            Throwable t = new Throwable();
-            System.out.println(t.getMessage());
-            t.getCause();
-            System.out.println(t.getClass());
-            t.getStackTrace();
-            t.printStackTrace();
-
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNext()){
+            int n = scan.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++){
+                arr[i] = scan.nextInt();
+            }
+            System.out.println(Arrays.toString(decompressRLElist(arr)));
         }
     }
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
@@ -37,16 +33,46 @@ public class MainMethods {
 
         return 0;
     }
-    public int[] decompressRLElist(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        int[] arr = new int[0];
+    public static int[] decompressRLElist(int[] nums) {
+        int sum = 0;
         for(int i = 0; i < nums.length; i+=2){
-            for(int j = 0; j < nums[i]; j++){
-                arr[j++]=nums[i+1];
+            sum+=nums[i];
+        }
+        int[] arr = new int[sum];
+        int j = 0;
+        for(int i = 0; i < nums.length; i+=2){
+            while(nums[i]!=0){
+                arr[j] = nums[i+1];
+                j++;
+                nums[i]--;
             }
         }
-        Map<Integer, String> map = new HashMap<>();
-        Map<Integer, String> map2= new TreeMap<>();
-
+        return arr;
+    }
+    public boolean isMonotonic(int[] A) {
+        if(A.length==1) return true;
+        boolean inc = true;
+        boolean dec = true;
+        for(int i = 0; i < A.length-1; i++){
+            if(A[i] < A[i+1]) dec = false;
+            if(A[i] > A[i+1]) inc = false;
+        }
+        return dec || inc;
+    }
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            if(map.containsKey(num)){
+                map.put(num, map.get(num)+1);
+            }else{
+                map.put(num, 1);
+            }
+        }
+//        int[] arr = new int[nums.length];
+        List<Integer> list = new ArrayList<>();
+        for(int key : map.keySet()){
+            
+        }
+        return null;
     }
 }
