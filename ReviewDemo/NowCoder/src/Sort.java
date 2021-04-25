b2a2b1eee538a88bf592dee3e7cb9bb4c63d33e8
Nero951 <zhlwd103511@gmail.com>
2021-04-25 12:08:29 +0800

    reviewdemo

--- ReviewDemo/NowCoder/src/Sort.java
+++ ReviewDemo/NowCoder/src/Sort.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sort {
@@ -9,17 +10,63 @@ public class Sort {
             for(int i = 0; i < n; i++){
                 arr[i] = scan.nextInt();
             }
+            mainSort s = new mainSort();
+//            s.bubbleSort(arr);
+//            s.quickSort(arr);
+            s.selectSort(arr);
+            System.out.println(Arrays.toString(arr));
         }
     }
 }
-class sort{
-    private void 
-    public void bubbleSort(int[] arr){
-        if(arr.length==0) return;
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr.length-i; j++){
-                if(arr[j]<arr[j+1]){
+class mainSort{
+    private void temp(int[] arr, int i, int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
 
+    public void bubbleSort(int[] arr){
+        if(arr.length<2) return;
+//        boolean flag = true;
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length-1-i; j++){
+                if(arr[j]<arr[j+1]){
+                    temp(arr, j, j+1);
+//                    flag = false;
+                }
+            }
+        }
+    }
+
+    public void quickSort(int[] arr){
+        if(arr.length<2) return;
+        quick(arr, 0, arr.length-1);
+    }
+    private void quick(int[] arr, int low, int high){
+        if(low < high){
+            int pivot = partition(arr, low, high);
+            quick(arr, low, pivot-1);
+            quick(arr, pivot+1, high);
+        }
+    }
+    private int partition(int[] arr, int low, int high){
+        int pivot = arr[low];
+        while(low < high){
+            while(low<high && arr[high]>=pivot) {--high;}
+            arr[low] = arr[high];
+            while(low<high && arr[low]<=pivot) {++low;}
+            arr[high] = arr[low];
+        }
+        arr[low] = pivot;
+        return low;
+    }
+
+    public void selectSort(int[] arr){
+        if(arr.length<2) return;
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i; j < arr.length; j++){
+                if(arr[j] < arr[i]){
+                    temp(arr, i, j);
                 }
             }
         }
